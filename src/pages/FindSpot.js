21764b159df1e97d3ea51a6b0bd97b8ca80850bb
Tinney Mak <tm0261@princeton.edu>
2023-07-01 01:10:09 +0800

    walking page updates 1

--- src/pages/FindSpot.js
+++ src/pages/FindSpot.js
@@ -1,10 +1,28 @@
 import '../App.css';
+import Navbar from '../components/Navbar';
+import firestone from "../images/firestone.png";
+import WalkButton from '../components/WalkButton';
+import RestartButton from '../components/RestartButton';
 
 export default function FindSpot() {
     return (
-      <a href="/findSpot" className = "findspot-container"> 
-          <h1>Find study spot</h1>
-      </a>
+      <>
+        <Navbar />
+        <div className = "findspot-container">
+            <img className = "image-container" src = {firestone}></img>
+            <div className = "text-container">
+                <h1 className = "location-name">FIRESTONE LIBRARY</h1>
+                <p className = "location-blurb">A classic... take the trek up to the top floor or hole up in the basement for the grind!</p>
+                <div className = "walk-buttons-container">
+                    <RestartButton className = "study-button walk-icon"/>
+                    <a href = "/study">
+                      <WalkButton />
+                    </a>
+                </div>
+            </div>
+        </div>
+      </>
+      
     );
   }
   
