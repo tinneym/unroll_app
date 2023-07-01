8b5e5ec0a55093b1c63ceeb53a2352f677eb670b
Tinney Mak <tm0261@princeton.edu>
2023-07-01 17:46:18 +0800

    find spot working

--- src/pages/FindSpot.js
+++ src/pages/FindSpot.js
@@ -1,20 +1,31 @@
 import '../App.css';
 import Navbar from '../components/Navbar';
-import firestone from "../images/firestone.png";
+import { useState } from 'react';
 import WalkButton from '../components/WalkButton';
 import RestartButton from '../components/RestartButton';
+import locationData from './locationData';
 
 export default function FindSpot() {
+    const locationsArray = locationData.studySpots;
+    const defaultNum = Math.floor(Math.random() * locationsArray.length);
+
+    const [id, setId] = useState(defaultNum);
+    
+    function generate() {
+      let randomNum = Math.floor(Math.random() * locationsArray.length);
+      setId(randomNum);
+    }
+
     return (
       <>
         <Navbar />
         <div className = "findspot-container">
-            <img className = "image-container" src = {firestone}></img>
+            <img className = "image-container" src = {require("../images/img" + id + ".png")}></img>
             <div className = "text-container">
-                <h1 className = "location-name">FIRESTONE LIBRARY</h1>
-                <p className = "location-blurb">A classic... take the trek up to the top floor or hole up in the basement for the grind!</p>
+                <h1 className = "location-name">{locationsArray[id].name}</h1>
+                <p className = "location-blurb">{locationsArray[id].blurb}</p>
                 <div className = "walk-buttons-container">
-                    <RestartButton className = "study-button walk-icon"/>
+                    <RestartButton onClick = {generate} className = "study-button walk-icon"/>
                     <a href = "/study">
                       <WalkButton />
                     </a>
---
+++ src/pages/locationData.js
@@ -0,0 +1,19 @@
+export default {
+    "studySpots": [
+        {
+            "id": "0",
+            "name": "FIRESTONE LIBRARY",
+            "blurb": "A classic... take the trek up to the top floor or hole up in the basement for the grind!"
+        },
+        {
+            "id": "1",
+            "name": "FRIST LATE MEAL",
+            "blurb": "Great if you're craving some social interaction or chicken tenders!"
+        },
+        {
+            "id": "2",
+            "name": "STOKES LIBRARY",
+            "blurb": "Centrally located, a quiet study spot that's a little lesser known!"
+        }
+    ]
+}
