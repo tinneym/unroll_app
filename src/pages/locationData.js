20718a1bffea3ab8bf5e0098308d50dd59af39f3
Tinney Mak <tm0261@princeton.edu>
2023-07-01 21:29:36 +0800

    dataset updates

--- src/pages/locationData.js
+++ src/pages/locationData.js
@@ -8,12 +8,201 @@ export default {
         {
             "id": "1",
             "name": "FRIST LATE MEAL",
-            "blurb": "Great if you're craving some social interaction or chicken tenders!"
+            "blurb": "Great if you're craving some social interaction and chicken tenders!"
         },
         {
             "id": "2",
             "name": "STOKES LIBRARY",
-            "blurb": "Centrally located, a quiet study spot that's a little lesser known!"
-        }
+            "blurb": "A peaceful spot good for quiet studying and people watching!"
+        },
+        {
+            "id": "3",
+            "name": "MATHEY COMMON ROOM",
+            "blurb": "A+ ambience... come for the tall windows, comfy couches, and occasional piano playing!"
+        },{
+            "id": "4",
+            "name": "ROCKY COMMON ROOM",
+            "blurb": "The cozy counterpart to the Mathey common room!"
+        },{
+            "id": "5",
+            "name": "COFFEE CLUB (PROSPECT)",
+            "blurb": "Boost your productivity with some caffeine and pastries!"
+        },{
+            "id": "6",
+            "name": "ADDY COMMON ROOM",
+            "blurb": "Modern-feeling, lots of natural light, and houses the other Coffee Club!"
+        },{
+            "id": "7",
+            "name": "YEH SIGNATURE SPACE",
+            "blurb": "Incredibly bright atmosphere with a ping pong and pool table nearby for study breaks!"
+        },{
+            "id": "8",
+            "name": "BUTLER BASEMENT",
+            "blurb": "Explore the numerous cool study spaces while also grabbing snacks at Studio 34!"
+        },{
+            "id": "9",
+            "name": "MCGRAW",
+            "blurb": "Hole up here for your pset grinds and finals studying!"
+        },{
+            "id": "10",
+            "name": "FINE HALL",
+            "blurb": "Channel your inner mathematician with the blackboards!"
+        },{
+            "id": "11",
+            "name": "NEURO BUILDING",
+            "blurb": "Snag an empty classroom or flip through your readings in the couch clusters!"
+        },{
+            "id": "12",
+            "name": "LEWIS LIBRARY",
+            "blurb": "Home of the blessed lab TAs - don't miss the Treehouse!"
+        },{
+            "id": "13",
+            "name": "LEWIS CENTER FOR THE ARTS",
+            "blurb": "Settle into one of the many armchairs in the atrium and absorb the creativity!"
+        },{
+            "id": "14",
+            "name": "MCCOSH HALL",
+            "blurb": "Lots of available classrooms where you can simulate experiencing your lecture again!"
+        },{
+            "id": "15",
+            "name": "JUNBI",
+            "blurb": "Take a trip to Nassau and treat yourself to some matcha!"
+        },{
+            "id": "16",
+            "name": "FIRESTONE UPPER FLOORS",
+            "blurb": "Go early to get a spot in the famed tower room!"
+        },{
+            "id": "17",
+            "name": "CHANCELLOR GREEN",
+            "blurb": "Peak silence levels + beautiful stained glass and architecture!"
+        },{
+            "id": "18",
+            "name": "YOUR DORM ROOM",
+            "blurb": "Good for pacing around, reciting lecture notes, and releasing built-up stress!"
+        },{
+            "id": "19",
+            "name": "HOLDER COURTYARD",
+            "blurb": "Get some sunlight while reading in a lawnchair!"
+        },{
+            "id": "20",
+            "name": "CANNON GREEN",
+            "blurb": "Bring out a picnic blanket and mix up your surroundings with some nature!"
+        },{
+            "id": "21",
+            "name": "MURRAY DODGE CAFE",
+            "blurb": "One word... cookies!"
+        },{
+            "id": "22",
+            "name": "FRIST CLASSROOMS",
+            "blurb": "Assemble the study group and go through practice exams on the big screen!"
+        },{
+            "id": "23",
+            "name": "EAS LIBRARY",
+            "blurb": "Very nice wood decor, seminar rooms, and other spacious study spaces!"
+        },{
+            "id": "24",
+            "name": "JRR SEMINAR ROOMS",
+            "blurb": "Great for hosting last minute review sessions!"
+        },{
+            "id": "25",
+            "name": "SPIA BOWL",
+            "blurb": "Good for larger study groups - bring snacks!"
+        },{
+            "id": "26",
+            "name": "NEW SOUTH",
+            "blurb": "Check out the top floor room for an amazing view of campus!"
+        },{
+            "id": "27",
+            "name": "PROSPECT GARDEN",
+            "blurb": "Pretty flowers and nice benches!"
+        },{
+            "id": "28",
+            "name": "MCCOSH COURTYARD",
+            "blurb": "Step outside and get some fresh air!"
+        },{
+            "id": "29",
+            "name": "ROCKY MATHEY LIBRARY",
+            "blurb": "The study room in the back is 10/10!"
+        },{
+            "id": "30",
+            "name": "NCW DINING HALL",
+            "blurb": "Hotel lobby atmosphere!"
+        },{
+            "id": "31",
+            "name": "MCDONNELL HALL",
+            "blurb": "Blackboards for working out your pset problems!"
+        },{
+            "id": "32",
+            "name": "JONES HALL",
+            "blurb": "A hidden gem!"
+        },{
+            "id": "33",
+            "name": "CAMPUS CLUB",
+            "blurb": "Alternatively, can go to your eating club nearby!"
+        },{
+            "id": "34",
+            "name": "CARL ICAHN BUILDING",
+            "blurb": "Very spacious and large windows for gazing upon Poe Field!"
+        },{
+            "id": "35",
+            "name": "FRIEND CENTER",
+            "blurb": "A bit of a walk, but you're sure to find a room!"
+        },{
+            "id": "36",
+            "name": "BLOOMBERG STUDY ROOMS",
+            "blurb": "Would highly recommend for study groups!"
+        },{
+            "id": "37",
+            "name": "GREEN HALL",
+            "blurb": "Has nice armchair clusters and classrooms!"
+        },{
+            "id": "38",
+            "name": "JADWIN HALL",
+            "blurb": "Physics kids know this place too well - stimulate your brain cells!"
+        },{
+            "id": "39",
+            "name": "FORBES DINING HALL",
+            "blurb": "Drop by Wawa for midnight snacks!"
+        },{
+            "id": "40",
+            "name": "PRINCETON PUBLIC LIBRARY",
+            "blurb": "Stretch your legs and book yourself a study room!"
+        },{
+            "id": "41",
+            "name": "HOLDER BASEMENT",
+            "blurb": "Check out the cozy study rooms or make instant ramen in the kitchen!"
+        },{
+            "id": "42",
+            "name": "ROCKY MATHEY DINING HALL",
+            "blurb": "Take your pick of the many long tables - also, fireplace!"
+        },{
+            "id": "43",
+            "name": "STARBUCKS",
+            "blurb": "It's Starbucks!"
+        },{
+            "id": "44",
+            "name": "SMALL WORLD",
+            "blurb": "Get the cafe productivity and much-needed coffee!"
+        },{
+            "id": "45",
+            "name": "WHITMAN LIBRARY",
+            "blurb": "A popular spot for late-night grinds!"
+        },{
+            "id": "46",
+            "name": "WHITMAN COURTYARD",
+            "blurb": "Nice outdoor tables with overlooking-people-from-my-castle vibes!"
+        },{
+            "id": "47",
+            "name": "FIRESTONE COURTYARD",
+            "blurb": "Don't descent into its depths just yet!"
+        },{
+            "id": "48",
+            "name": "MENDEL MUSIC LIBRARY",
+            "blurb": "If you're looking for a bit of musical background noise!"
+        },{
+            "id": "49",
+            "name": "EDWARDS BASEMENT",
+            "blurb": "Houses a very large beanbag!"
+        },
     ]
 }
