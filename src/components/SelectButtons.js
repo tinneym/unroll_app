1566dd0a6f50ed2c45739748be5b96c0ab23d539
tinneym <tm0261@princeton.edu>
2023-07-03 11:42:09 +0800

    fixes

--- src/components/SelectButtons.js
+++ src/components/SelectButtons.js
@@ -1,6 +1,3 @@
-import { useContext, useState } from "react";
-import { TimeContext } from "./TimeContext";
-
 export default function SelectButtons(props) {   
     function changeTime() {
         props.setTime(props.time);
